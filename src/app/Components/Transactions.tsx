897d86ec688006e18469c7e5bf9e05bcec8e620f
Hikmat Ullah <hikmatcse1919@gmail.com>
2023-06-04 16:59:09 +0500

    Global state

---
+++ src/app/Components/Transactions.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+export const Transactions = () => {
+    
+  return (
+    <div>
+        
+                <li className="flex border-r-4 justify-between border-r-green-700 items-center px-10 space-x-5 w-full h-12 bg-slate-50 hover:bg-green-700">
+                    <p >income1</p>
+                    <span >+$100</span>
+                    
+                </li>
+
+                {/* <li className="flex border-r-4 justify-between border-r-orange-600 items-center px-10 space-x-5 w-full h-12 bg-slate-50 hover:bg-orange-600">
+                    <p >expense</p>
+                    <span >+$100</span>
+                </li> */}
+
+                
+        
+    </div>
+  )
+}
--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -1,9 +1,16 @@
-import React , {useContext , useReducer} from 'react'
 
-export const GlobalState = () => {
-  return (
-    <div>
-        <h1>hello</h1>
-    </div>
-  )
-}
+import React , {useContext , useReducer} from 'react'
+import { createContext } from 'vm'
+
+
+const initialTransactions = [
+  {amount: 50 , desc: "cash"},
+  {amount: -650 , desc: "laptop"},
+  {amount: -350 , desc: "books"}
+]
+
+
+//to access all components
+export const TransactionContext = createContext(initialTransactions);
+
+
