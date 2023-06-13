ac494ca2451708bfe05ed6089dfdaf96a3985871
Hikmat Ullah <hikmatcse1919@gmail.com>
2023-06-14 00:19:31 +0500

    expense changed

--- src/app/Components/Transactions.tsx
+++ src/app/Components/Transactions.tsx
@@ -15,7 +15,7 @@ export const Transactions = () => {
 
                 <li className="flex border-r-4 justify-between border-r-orange-600 items-center px-10 space-x-5 w-full h-12 bg-slate-50 hover:bg-orange-600">
                     <p >expense</p>
-                    <span >+$100</span>
+                    <span >+$50</span>
                 </li>
 
                 
